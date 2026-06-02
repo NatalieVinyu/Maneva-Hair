abf721083aa091f091ec4f16c96f3ae0db87301a
Natalie Vinyu <135601983+NatalieVinyu@users.noreply.github.com>
2026-06-02 14:08:54 +0200

    fix: set up React Router with BrowserRouter and Routes in App.jsx

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,16 +1,18 @@
-import React from 'react';
-import Navbar from './components/Navbar';
-import Home from './pages/Home';
-import About from './pages/About';
+import React from 'react'
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import Navbar from './components/Navbar'
+import Home from './pages/Home'
+import About from './pages/About'
 
 function App() {
   return (
-    <div>
+    <BrowserRouter>
       <Navbar />
-      <Home />
-      <About />
-      
-    </div>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
+      </Routes>
+    </BrowserRouter>
   )
 }
 
