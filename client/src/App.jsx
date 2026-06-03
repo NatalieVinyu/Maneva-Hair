ad920e784fc2899af30fc618cca3cbc9ae841345
Natalie Vinyu <135601983+NatalieVinyu@users.noreply.github.com>
2026-06-03 11:18:24 +0200

    feat: imported pages and linked to Route

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -3,15 +3,25 @@ import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import Navbar from './components/Navbar'
 import Home from './pages/Home'
 import About from './pages/About'
+import Shop from './pages/Shop'
+import Services from './pages/Services'
+import Gallery from './pages/Gallery'
+import Footer from './components/Footer'
 
 function App() {
   return (
     <BrowserRouter>
       <Navbar />
+
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/about" element={<About />} />
+        <Route path="/shop" element={<Shop />} />
+        <Route path="/services" element={<Services />} />
+        <Route path="/gallery" element={<Gallery />} />
+
       </Routes>
+      <Footer />
     </BrowserRouter>
   )
 }
