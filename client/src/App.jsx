96648c4f3048e8e218008d3b7202f109d3076447
Natalie Vinyu <135601983+NatalieVinyu@users.noreply.github.com>
2026-06-03 14:59:41 +0200

    add ScrollToTop action

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,17 +1,20 @@
 import React from 'react'
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import Navbar from './components/Navbar'
-import Booking from './components/Booking'
+import Booking from './pages/Booking'
 import Home from './pages/Home'
 import About from './pages/About'
 import Shop from './pages/Shop'
 import Services from './pages/Services'
 import Gallery from './pages/Gallery'
 import Footer from './components/Footer'
+import ScrollToTop from './components/ScrollToTop'
 
 function App() {
   return (
     <BrowserRouter>
+      <ScrollToTop />
+      
       <Navbar />
 
       <Routes>
